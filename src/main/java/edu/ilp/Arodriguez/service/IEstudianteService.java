8397071a12570f6656660318a3612f3203eb191b
kllisman <89989898+kllisman@users.noreply.github.com>
2022-04-28 22:27:17 -0500

    "obtener estudiante por codigo"

--- src/main/java/edu/ilp/Arodriguez/service/IEstudianteService.java
+++ src/main/java/edu/ilp/Arodriguez/service/IEstudianteService.java
@@ -4,5 +4,5 @@ import edu.ilp.Arodriguez.entity.Estudiante;
 
 public interface IEstudianteService {
     //listar estudiante por codigo
-    Estudiante obtenerEstudiantePorCodigo(String codigo);
+   // Estudiante obtenerEstudiantePorCodigo(String codigo);
 }
